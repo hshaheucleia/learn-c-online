8b8b19ae64b780a51b056dbbc3b8c929b5234c83
Benjamin James Wirght <bwright@cse.unsw.edu.au>
2013-08-27 00:31:15 +1000

    Added support to compile C to js via an IPC message link to the OL home base

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import mechanize
 from flask import Flask, jsonify, request, render_template
 import json
 
@@ -9,11 +10,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+def compile_message(code):
+    br = mechanize.Browser()
+    br.open("http://compile.openlearningapps.net/application/upload")
+    br.select_form(nr=0)
+    open('main.c', 'w').write(code)
+    br.form.add_file(open('main.c'), 'text/plain', 'main.c', name='file1')
+    response = br.submit()
+    return response.read()
+
 @app.route("/api/code/submit", methods=["PUT", "POST"])
 def submit():
   try:
     # Attempt to evaluate the expression.
-    return jsonify({'result': str(request.json['code'])})
+    return jsonify({'result': compile_message(str(request.json['code']))})
   # If something borks, just return a catch all.
   except:
     return jsonify({'error': 'invalid code expression'})
