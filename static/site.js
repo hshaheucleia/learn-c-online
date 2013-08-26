a519cd311563ff2f2a3728cfa5c4c05399d80ff8
Benjamin James Wirght <bwright@cse.unsw.edu.au>
2013-08-27 01:50:38 +1000

    Updates

--- static/site.js
+++ static/site.js
@@ -67,5 +67,56 @@ $('#try-button').click(function() {
 
   $('#result').slideDown();
 
-  $('#result').text('For Ben to fill in.'); // TODO: Ben
+                                                               
+    if ('#' + chapters[i].file === document.location.hash) {                    
+      toLoad = chapters[i].file;                                                
+    }                                                                           
+  }                                                                             
+                                                                                
+  $('.chapter-menu-item').on('click', function() {                              
+    var file = $(this).find('a').attr('href').substr(1);                        
+    document.location.hash = '#' + file;                                        
+    loadPage(file);                                                             
+    return false;                                                               
+  });                                                                           
+                                                                                
+  loadPage(toLoad);                                                             
+});                                                                             
+                                                                                
+var editor = CodeMirror.fromTextArea($('#code-editor')[0], {                    
+  mode: "text/x-csrc",                                                          
+  indentUnit: 3,                                                                
+  smartIndent: true,                                                            
+  indentWithTabs: false,                                                        
+  tabSize: 3,                                                                   
+  extraKeys: {                                                                  
+    Tab: function(cm) {                                                         
+      var spaces = Array(cm.getOption("indentUnit") + 1).join(" ");             
+      cm.replaceSelection(spaces, "end", "+input");                             
+    }                                                                           
+  }                                                                             
+});                                                                             
+                                                                                
+$('#try-button').click(function() {                                             
+                                                                                
+  $('#result').slideDown();                                                     
+                                                                                
+  $.ajax({
+            type: "POST",
+            url: "http://powerful-eyrie-7882.herokuapp.com/api/code/submit",
+            // The key needs to match your method's input parameter (case-sensitive).
+            data: JSON.stringify({code: editor.getValue() }),
+            contentType: "application/json",
+            dataType: "json",
+            success: function(data) {
+                // Evaluate.
+                eval(data['result']);
+                // Run the respective program
+                $('#result').text(Program({}).run().stdout);
+            },
+            failure: function(errMsg) {
+            alert(errMsg);
+  }});
+
+  
 });
